75ce8d2798fbbc10c87d067c2937beda470101a9
Willem_Windows <wimjoost@gmail.com>
2023-10-13 14:59:09 +0200

    UTILS-1 clean up

---
+++ docker-services/src/main/java/com/general/utils/docker/container/ContainerRequest.java
@@ -0,0 +1,9 @@
+package com.general.utils.docker.container;
+
+public class ContainerRequest extends Container {
+
+    public ContainerRequest(String baseImage, String name, String version) {
+	super(baseImage, name, version);
+    }
+
+}
