4cafb0da0abd706aceb34c03619493e89e7199bc
Willem_Windows <wimjoost@gmail.com>
2023-11-17 09:05:28 +0100

    UTILS-11 add test

---
+++ object-utils/src/test/java/com/general/utils/object/mappers/ObjectToMapMapperTest.java
@@ -0,0 +1,19 @@
+package com.general.utils.object.mappers;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ObjectToMapMapperTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testConvertObjectToMap() {
+	assertTrue(true);
+    }
+
+}
