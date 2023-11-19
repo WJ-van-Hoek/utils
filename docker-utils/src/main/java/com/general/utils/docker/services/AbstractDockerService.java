49323ca504ed7d758b7347936990c2c43e47c7af
Willem_Windows <wimjoost@gmail.com>
2023-11-19 11:26:08 +0100

    UTILS-19 fix checkstyle part 3

--- docker-utils/src/main/java/com/general/utils/docker/services/AbstractDockerService.java
+++ docker-utils/src/main/java/com/general/utils/docker/services/AbstractDockerService.java
@@ -12,18 +12,41 @@ package com.general.utils.docker.services;
 
 import com.general.utils.docker.client.DockerClientConfigFactory;
 import com.general.utils.docker.client.DockerHttpClientFactory;
-import com.general.utils.docker.container.ContainerResponse;
+import com.general.utils.docker.container.Container;
 import com.general.utils.docker.container.Launcher;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.core.DockerClientBuilder;
 import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.transport.DockerHttpClient;
 
+/**
+ * The {@code AbstractDockerService} class is an abstract implementation of the
+ * {@code DockerService} interface. It provides common functionality for
+ * interacting with Docker, such as creating a Docker client and launching
+ * containers. Concrete classes extending this abstract class must implement
+ * additional Docker-related functionality.
+ *
+ * <p>
+ * The Docker client is initialized during the construction of this class using
+ * the provided Docker host information. The client is used to communicate with
+ * the Docker daemon for various operations.
+ *
+ * @see DockerService
+ */
 public abstract class AbstractDockerService implements DockerService {
 
+    /**
+     * The Docker client used for interacting with the Docker daemon.
+     */
     private final DockerClient dockerClient;
 
-    protected AbstractDockerService(String dockerHost) {
+    /**
+     * Constructs an {@code AbstractDockerService} with the specified Docker
+     * host.
+     *
+     * @param dockerHost The address of the Docker host to connect to.
+     */
+    protected AbstractDockerService(final String dockerHost) {
         DockerClientConfig config = DockerClientConfigFactory
                 .createDockerClientConfig(dockerHost);
         DockerHttpClient client = DockerHttpClientFactory
@@ -32,8 +55,19 @@ public abstract class AbstractDockerService implements DockerService {
                 .withDockerHttpClient(client).build();
     }
 
-    public ContainerResponse launchContainer(String image, String name,
-            String version) {
+    /**
+     * Launches a Docker container using the initialized Docker client.
+     * Delegates the container launch to the {@link Launcher} class.
+     *
+     * @param image The name of the Docker image to be used for the container.
+     * @param name The desired name for the Docker container.
+     * @param version The version tag for the Docker image.
+     * @return A {@code Container} object representing the launched Docker
+     * container.
+     * @see Launcher#launchContainer(DockerClient, String, String, String)
+     */
+    public Container launchContainer(final String image, final String name,
+            final String version) {
         return Launcher.launchContainer(this.dockerClient, image, name,
                 version);
     }
--- docker-utils/src/main/java/com/general/utils/docker/services/DockerService.java
+++ docker-utils/src/main/java/com/general/utils/docker/services/DockerService.java
@@ -10,9 +10,29 @@
  */
 package com.general.utils.docker.services;
 
-import com.general.utils.docker.container.ContainerResponse;
+import com.general.utils.docker.container.Container;
 
+/**
+ * The {@code DockerService} interface defines the contract for interacting with
+ * Docker services. Classes implementing this interface are expected to provide
+ * functionality related to Docker containers, such as launching containers with
+ * specific images and configurations.
+ *
+ * <p>
+ * Implementing classes must provide a concrete implementation for the
+ * {@link #launchContainer(String, String, String)} method, specifying the
+ * details of how Docker containers are launched.
+ *
+ */
 public interface DockerService {
-    ContainerResponse launchContainer(String image, String name,
-            String version);
+    /**
+     * Launches a Docker container with the specified parameters.
+     *
+     * @param image The name of the Docker image to be used for the container.
+     * @param name The desired name for the Docker container.
+     * @param version The version tag for the Docker image.
+     * @return A {@code Container} object representing the launched Docker
+     * container.
+     */
+    Container launchContainer(String image, String name, String version);
 }
